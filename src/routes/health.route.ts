c3ee1b312789d7c721496bc23a128ee23cde9984
Saeed-60307149 <darabdullah004@gmail.com>
2026-04-27 12:58:12 +0300

    Changed the msg in /health to train how does the routing work

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -5,7 +5,7 @@ export async function healthRoutes(app: FastifyInstance) {
     app.get("/health", async (): Promise<IService> => {
         return {
             success: true,
-            message: "Hello World!",
+            message: "Hello World! I am Saeed and i am just starting to train on fastify",
             data: {
                 time: new Date().toISOString(),
                 timeZone: "Asia/Qatar"
